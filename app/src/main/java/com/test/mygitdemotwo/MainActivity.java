0e6e04854df5d9c5b08f29b466662e9fdca47e1d
lmq <lmq@example.com>
2018-01-18 00:12:46 +0800

    version2.01

--- app/src/main/java/com/test/mygitdemotwo/MainActivity.java
+++ app/src/main/java/com/test/mygitdemotwo/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //在分支二上进行的修改
+        getWindowManager().getDefaultDisplay();
     }
 }
